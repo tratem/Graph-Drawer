d6ce7abe64de671871fa23377a2b9dc3c8ff62d8
tratem <trajche.temelkovski@outlook.com>
2025-04-21 11:03:32 +0200

    ADDED: - widgets config for signle or dual axis selection

--- GraphDrawer.py
+++ GraphDrawer.py
@@ -1,6 +1,7 @@
 import csv
 import sys
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QListWidget, QListWidgetItem,
+                             QVBoxLayout, QHBoxLayout)
 from PyQt5.QtCore import QSettings, Qt
 from CustomWidgets import custom_widgets as CW
 
@@ -14,7 +15,7 @@ class MainWindow(QMainWindow):
     def init_UI(self):
         main_layout = QVBoxLayout()
         file_select_layout = QHBoxLayout() # selected file and select file button
-        self.axis_type_layout = QVBoxLayout() # content change dependant on axis_type_combo selection
+        self.axis_dependant_layout = QVBoxLayout() # content change dependant on axis_type_combo selection
 
         central_widget = QWidget()
         central_widget.setLayout(main_layout)
@@ -44,7 +45,7 @@ class MainWindow(QMainWindow):
         self.handle_single_axis_selection() #Set single as initial config
         main_layout.addWidget(axis_type_combo)
 
-        main_layout.addLayout(self.axis_type_layout)
+        main_layout.addLayout(self.axis_dependant_layout)
 
         plot_button = CW.create_button("PLOT", "black", "lime")
         plot_button.clicked.connect(self.handle_plot_button_click)
@@ -60,10 +61,35 @@ class MainWindow(QMainWindow):
             self.handle_dual_axis_selection()
 
     def handle_single_axis_selection(self):
-        pass
+        '''Change layout when single axis is selected'''
+        CW.clear_layout(self.axis_dependant_layout)
+        self.left_y_items_list = QListWidget()
+        self.left_y_axis_label_line_edit = CW.create_line_edit("Left y axis label..")
+        self.axis_dependant_layout.addWidget(self.left_y_items_list)
+        self.axis_dependant_layout.addWidget(self.left_y_axis_label_line_edit)
 
     def handle_dual_axis_selection(self):
-        pass
+        '''Change layout when dual axis is selected'''
+        CW.clear_layout(self.axis_dependant_layout)
+        dual_axis_layout = QHBoxLayout()
+        left_axis_layout = QVBoxLayout()
+        right_axis_layout = QVBoxLayout()
+
+        self.left_y_items_list = QListWidget()
+        self.left_y_axis_label_line_edit = CW.create_line_edit("Left y axis label..")
+        left_axis_layout.addWidget(self.left_y_items_list)
+        left_axis_layout.addWidget(self.left_y_axis_label_line_edit)
+
+        
+        self.right_y_items_list = QListWidget()
+        self.right_y_axis_label_line_edit = CW.create_line_edit("Right y axis label..")
+        right_axis_layout.addWidget(self.right_y_items_list)
+        right_axis_layout.addWidget(self.right_y_axis_label_line_edit)
+
+        dual_axis_layout.addLayout(left_axis_layout)
+        dual_axis_layout.addLayout(right_axis_layout)
+
+        self.axis_dependant_layout.addLayout(dual_axis_layout)
 
     def handle_plot_button_click(self):
-        pass
+        '''Show Figure'''
